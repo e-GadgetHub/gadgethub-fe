1d75c766b8226ee33c3b1d2e11eeff7a6dca7616
NubieMe <fahmi.mellow@gmail.com>
2024-04-25 16:07:05 +0700

    lol

--- src/component/Navbar/index.tsx
+++ src/component/Navbar/index.tsx
@@ -1,39 +1,43 @@
-import { Box, Image, Input, InputGroup, InputLeftElement, Text } from '@chakra-ui/react'
-import { FaSearch, FaShoppingCart } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Box, Image, Input, InputGroup, InputLeftElement, Text } from "@chakra-ui/react";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <Box display={'flex'} h={'75px'} justifyContent={'center'} alignItems={'center'} borderBottom={'1px solid'} bg={'transparent'}>
-        <Box  w={'90%'} display={'flex'} justifyContent={'space-between'}>
-            <Box w={'20%'}>
-                <Link to={'/'}>
-                    <Image src='../../../src/assets/image/logo-e-comers.png'/>
-                </Link>
-                    
+    return (
+        <Box
+            display={"flex"}
+            h={"75px"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            borderBottom={"1px solid"}
+            bg={"transparent"}
+        >
+            <Box w={"90%"} display={"flex"} justifyContent={"space-between"}>
+                <Box w={"20%"}>
+                    <Link to={"/"}>
+                        <Image src="../../../src/assets/image/logo-e-comers.png" />
+                    </Link>
+                </Box>
+                <Box display={"flex"} w={"60%"} gap={"25px"} justifyContent={"center"} alignItems={"center"}>
+                    <Link to={"/"}>
+                        <Text>Home</Text>
+                    </Link>
+                    <Text>Contact</Text>
+                    <Text>About</Text>
+                    <InputGroup bg={"white"} w={"50%"}>
+                        <InputLeftElement>
+                            <FaSearch />
+                        </InputLeftElement>
+                        <Input type="text"></Input>
+                    </InputGroup>
+                </Box>
+                <Box display={"center"} w={"20%"} justifyContent={"center"} alignItems={"center"} gap={5}>
+                    <FaShoppingCart size={30} />
+                    <Link to={"/register"}>
+                        <Text>Register</Text>
+                    </Link>
+                </Box>
             </Box>
-            <Box display={'flex'} w={'60%'} gap={'25px'} justifyContent={'center'} alignItems={'center'} >
-                <Link to={'/'}>
-                    <Text>Home</Text>
-                </Link>
-                <Text>Contact</Text>
-                <Text>About</Text>
-                <InputGroup bg={'white'} w={'50%'}>
-                    <InputLeftElement>
-                        <FaSearch />
-                    </InputLeftElement>
-                    <Input type='text' ></Input>
-                </InputGroup>
-
-            </Box>
-            <Box display={'center'} w={'20%'} justifyContent={'center'} alignItems={'center'} gap={5}>
-                <FaShoppingCart size={30} />
-                <Link to={'/register'}>
-                <Text>Login</Text>
-                </Link>
-            </Box>
-
         </Box>
-    </Box>
-  )
-}
+    );
+};
