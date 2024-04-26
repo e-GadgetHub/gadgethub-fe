6024c43703374fe37ec19fbc75ba5d24b056e471
NubieMe <fahmi.mellow@gmail.com>
2024-04-26 10:11:38 +0700

    update: update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Home } from './pages/Home/Home'
-import { Login } from './pages/login/Login'
-import { Register } from './pages/register/Register'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Layout from "./layout";
 
 function App() {
-  
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
