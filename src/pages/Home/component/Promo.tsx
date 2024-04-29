a70c5788503f4beaf856cdbc78dfbf5b8e32a53f
nikri009 <nikriaria@gmail.com>
2024-04-29 10:14:56 +0700

    feat: Swiper Slide in home page

--- src/pages/Home/component/Promo.tsx
+++ src/pages/Home/component/Promo.tsx
@@ -1,37 +1,60 @@
-import { Box, Image, Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
-import dumy from '../../../mock/reqProduct.json'
-import { Ireq } from '../../../type'
+import { Box, Image, Text } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import data from "../../../mock/reqProduct.json";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 export const Promo = () => {
-    const [ data, setData ] = useState<Ireq>()
-    const [ currentIndex, setCurrentIndex ] = useState(0)
-  
-    useEffect(()=> {
-      setTimeout(()=>{
-        if ( currentIndex <= 2) {
-          setCurrentIndex((prev)=> prev + 1)
-        } 
-      }, 3000)
-  
-      if ( currentIndex > 2) {
-        setCurrentIndex(0)
-      }
-      setData(dumy[currentIndex])
-  
-    },[ currentIndex])
   return (
-    <Box display={'flex'} bgImage={'../../../../src/assets/image/BGCARD.jpeg'}  backgroundSize={'cover'} w={'85%'} rounded={'10px'} m={'20px'}  justifyContent={'space-between'}>
-    <Box justifyItems={'center'} p={'100px'} color={'white'}>
-      <Text fontSize={'35px'} fontWeight={'bold'}>{data?.name}</Text><br></br>
-      <Text>Spesifikasi : </Text>
-      <Text>Ram : {data?.spec?.ram}</Text>
-      <Text>Storage : {data?.spec?.storage}</Text>
-      <Text>Kamera : {data?.spec?.kamera}</Text>
-    </Box>
-    <Box display={'flex'} justifyContent={'center'}> 
-      <Image src={`../../${data?.image}`} w={'70%'} />
-    </Box>
-  </Box>
-  )
-}
+    <Swiper
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "auto",
+      }}
+      modules={[Navigation, Pagination, Autoplay]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000 }}
+      loop={true}
+      speed={1000}
+      centeredSlides={true} // Pindahkan prop centeredSlides ke sini
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {data.map((item, index) => (
+        <SwiperSlide key={index} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Box
+            display={"flex"}
+            bgImage={"../../../../src/assets/image/BGCARD.jpeg"}
+            backgroundSize={"cover"}
+            w={"85%"}
+            rounded={"10px"}
+            m={"20px"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            textAlign={"center"}
+          >
+            <Box justifyItems={"center"} p={"100px"} color={"white"}>
+              <Text fontSize={"35px"} fontWeight={"bold"}>
+                {item?.name}
+              </Text>
+              <br></br>
+              <Text>Spesifikasi : </Text>
+              <Text>Ram : {item?.spec?.ram}</Text>
+              <Text>Storage : {item?.spec?.storage}</Text>
+              <Text>Kamera : {item?.spec?.kamera}</Text>
+            </Box>
+            <Box display={"flex"} justifyContent={"center"}>
+              <Image src={`../../${item?.image}`} w={"70%"} />
+            </Box>
+          </Box>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,30 +7,43 @@ import { Promo } from "./component/Promo";
 import React from "react";
 
 const Component: React.FC = () => {
-    return (
-        <Box display={"grid"} justifyItems={"center"} gap={"10px"}>
-            <Promo />
+  return (
+    <Box
+      w={"100%"}
+      display={"grid"}
+      justifyItems={"center"}
+      gap={"10px"}
+    >
+      
 
-            <Box display={"flex"} w={"100%"} justifyContent={"center"} borderY={"1px"}>
-                <Category />
-            </Box>
+        <Promo />
+      
 
-            <Box w={"85%"} my={"20px"}>
-                <BestSelling />
-            </Box>
+      <Box
+        display={"flex"}
+        w={"100%"}
+        justifyContent={"center"}
+        borderY={"1px"}
+      >
+        <Category />
+      </Box>
 
-            <Box w={"85%"} minH={"600px"} my={"10px"}>
-                <Text fontWeight={"bold"} fontSize={"20px"}>
-                    New Arrival
-                </Text>
-                <NewAririval />
-            </Box>
+      <Box w={"85%"} my={"20px"}>
+        <BestSelling />
+      </Box>
 
-            <Box w={"85%"} my={"70px"}>
-                <Superiority />
-            </Box>
-        </Box>
-    );
+      <Box w={"85%"} minH={"600px"} my={"10px"}>
+        <Text fontWeight={"bold"} fontSize={"20px"}>
+          New Arrival
+        </Text>
+        <NewAririval />
+      </Box>
+
+      <Box w={"85%"} my={"70px"}>
+        <Superiority />
+      </Box>
+    </Box>
+  );
 };
 
 const Home = React.memo(Component);
