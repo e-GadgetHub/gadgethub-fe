6956b732b5f94ed91eed192c9f6450af08932a7a
unknown <febrilanianurislami@gmail.com>
2024-04-30 20:47:07 +0700

    create: page detail & page account

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import Home from "./pages/Home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Layout from "./layout";
+import DetailPage from "./pages/Detail/DetailPage";
+import AccountPage from "./pages/Account/AccountPage";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/account" element={<AccountPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
