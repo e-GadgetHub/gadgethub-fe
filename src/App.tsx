73081acd90e7470e9fc9bfd1e260888b91e5b005
NubieMe <fahmi.mellow@gmail.com>
2024-04-30 22:27:18 +0700

    add wishlist & not found routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,24 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Layout from "./layout";
 import Cart from "./pages/Cart";
+import NotFound from "./pages/notFound";
+import Wishlist from "./pages/wishlist";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/cart" element={<Cart />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
